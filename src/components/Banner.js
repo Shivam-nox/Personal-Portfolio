d0a67c45a6064266ee1a53c42aa2d8bf063a1a78
Shivam Dwivedi <shvm945@gmail.com>
2026-04-02 00:40:01 +0530

    Update link in Banner component

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -343,7 +343,7 @@ useEffect(() => {
 
             <div className="hero-cta-row">
               <a
-                href="https://drive.google.com/file/d/1jfyR2welA8BHQ84xRZ8Tv1Ml5OfZZ1WK/view?usp=sharing"
+                href="https://drive.google.com/file/d/1-FM_H712aUuvHFciYBZb3mR4eUJnfyRo/view?usp=sharing"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="btn-primary-hero"
